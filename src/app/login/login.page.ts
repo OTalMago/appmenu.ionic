a0c347492ae843c81f748a0d0424e24d55b0b35c
OTalMago <endcrow33@gmail.com>
2025-04-25 17:53:56 -0300

    Login validado

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  standalone:false,
+  standalone: false,  // Mantido sem alteração!
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  login: string = '';   // Variável para o login
+  senha: string = '';   // Variável para a senha
 
-  ngOnInit() {
+  constructor(private alertController: AlertController) { }
+
+  ngOnInit() { }
+
+  async entrar() {
+    if (this.login === 'Mago' && this.senha === '123456') {
+      const alert = await this.alertController.create({
+        header: 'Login realizado!',
+        message: 'Bem-vindo, Mago adm (admmago@gmail.com)',
+        buttons: ['OK']
+      });
+      await alert.present();
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Erro!',
+        message: 'Usuário ou senha inválidos!',
+        buttons: ['OK']
+      });
+      await alert.present();
+    }
   }
-
 }
